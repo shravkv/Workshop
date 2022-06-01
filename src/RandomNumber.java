2819c94122848e51991ce18ddd70bcfde7785d6e
sravan <sravan.k.vodnala@gmail.com>
2022-06-01 09:11:10 +0530

    random

---
+++ src/RandomNumber.java
@@ -0,0 +1,22 @@
+public class RandomNumber {
+
+     public static void main(String[] args) {
+
+         // generating random numbers between0 to 1
+         System.out.println("1st Random Number:"+Math.random());
+         System.out.println("2nd Random Number:"+Math.random());
+         System.out.println("3rd Random Number:"+Math.random());
+         System.out.println("4th Random Number:"+Math.random());
+         System.out.println("5th Random Number:"+Math.random());
+
+         // finding the average value
+         System.out.println();
+
+         // finding maximum value
+         System.out.println(Math.max());
+
+     }
+          // finding minimum value
+         System.out.println(Math.min());
+    }
+}
